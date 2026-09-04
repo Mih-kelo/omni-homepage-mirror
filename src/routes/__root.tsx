9d6d92789cd6937d8e7a728d74e097973260ed9f
Michael Okechukwu <mihkelo@proton.me>
2026-09-04 16:36:31 +0100

    feat: add Google tag (gtag.js) to root html head for analytics

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -142,6 +142,16 @@ function RootShell({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-MTXLCEKC54" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-MTXLCEKC54');`,
+          }}
+        />
         <HeadContent />
       </head>
       <body>
